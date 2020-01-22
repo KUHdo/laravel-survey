bb87a84f4bcfa5dd57548334523471f50a7ff05d
Ede <eduard.malyj@gmail.com>
2020-01-22 16:02:20 +0100

    Add test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kuhdo\Survey\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
