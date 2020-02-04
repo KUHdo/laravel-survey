767a9eb465834ee7e84371e5e1db13ac37223530
Ede <eduard.malyj@gmail.com>
2020-02-04 13:06:47 +0100

    Add tests for survey

--- src/Survey.php
+++ src/Survey.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Survey extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'surveys';
+
+    protected $fillable = [
+        'title'
+    ];
 }
---
+++ tests/Traits/WithSurvey.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Kuhdo\Survey\Tests\Traits;
+
+use Illuminate\Database\Eloquent\Collection;
+use Kuhdo\Survey\Survey;
+
+/**
+ * Trait WithSurvey
+ * @package Kuhdo\Survey\Tests\Traits
+ */
+trait WithSurvey
+{
+    /**
+     * @param array $attributes
+     * @return Survey
+     */
+    function makeSurvey($attributes = [])
+    {
+        $default = [
+            'title' => 'Test'
+        ];
+
+        $survey = new Survey(array_merge($default, $attributes));
+
+        return $survey;
+    }
+
+    /**
+     * @param array $attributes
+     * @return Survey
+     */
+    function createSurvey($attributes = [])
+    {
+        $survey = $this->makeSurvey($attributes);
+
+        $survey->save();
+
+        return $survey;
+    }
+
+    /**
+     * @param int $count
+     * @param array $attributes
+     * @return Collection
+     */
+    function makeSurveys($count = 1, $attributes = [])
+    {
+        $surveys = new Collection();
+
+        for ($i = 0; $i < $count; $i++) {
+            $surveys->push($this->makeSurvey($attributes));
+        }
+
+        return $surveys;
+    }
+
+    /**
+     * @param int $count
+     * @param array $attributes
+     * @return Collection
+     */
+    function createSurveys($count = 1, $attributes = [])
+    {
+        $surveys = $this->makeSurveys($count, $attributes);
+
+        $surveys->each(function ($survey) {
+            /** @var Survey $survey */
+           $survey->save();
+        });
+
+        return $surveys;
+    }
+}
