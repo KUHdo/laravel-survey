d227a306ac8306aa63541a4c056e8459c196e658
Ede <eduard.malyj@gmail.com>
2020-02-07 11:55:25 +0100

    Update test

--- tests/VoterTest.php
+++ tests/VoterTest.php
@@ -9,12 +9,18 @@ class VoterTest extends TestCase
 {
     use WithAnswer;
 
+    /**
+     * Should be correct object type of MorphMany
+     */
     public function testVoterIsAnswerable()
     {
         $user = new User();
         $this->assertInstanceOf(MorphMany::class, $user->answers());
     }
 
+    /**
+     * Should assign an answer to a voter (User)
+     */
     public function testAssociateAnswerWithVoter()
     {
         /** @var User $user */
@@ -26,7 +32,7 @@ class VoterTest extends TestCase
         $answer->model()->associate($user);
         $answer->save();
 
-        $this->assertEquals($answer->model()->first()->attributesToArray(), $user->attributesToArray());
-        $this->assertEquals($user->answers()->first()->attributesToArray(), $answer->attributesToArray());
+        $this->assertTrue($answer->model()->first()->is($user));
+        $this->assertTrue($user->answers()->first()->is($answer));
     }
 }
