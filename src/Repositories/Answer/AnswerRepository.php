4ba018024766ab37240f4d131f89310c12ca4745
Ede <eduard.malyj@gmail.com>
2020-02-10 15:11:35 +0100

    Add question related repository functions

--- src/Repositories/Answer/AnswerRepository.php
+++ src/Repositories/Answer/AnswerRepository.php
@@ -3,13 +3,35 @@
 
 namespace Kuhdo\Survey\Repositories\Answer;
 
+use Illuminate\Database\Eloquent\Builder;
 use Kuhdo\Survey\Contracts\Voter\Voteable as Voter;
+use Kuhdo\Survey\Question;
 
 interface AnswerRepository
 {
     /**
      * @param Voter $voter
+     * @return mixed
      */
-    public function getLatest(Voter $voter);
+    function getAll(Voter $voter);
 
+    /**
+     * @param Voter $voter
+     * @return mixed
+     */
+    function getLatest(Voter $voter);
+
+    /**
+     * @param Voter $voter
+     * @param Question $question
+     * @return mixed
+     */
+    function getAllOfQuestion(Voter $voter, Question $question);
+
+    /**
+     * @param Voter $voter
+     * @param Question $question
+     * @return mixed
+     */
+     function getLatestOfQuestion(Voter $voter, Question $question);
 }
--- tests/Unit/Answer/RepositoryTest.php
+++ tests/Unit/Answer/RepositoryTest.php
@@ -26,11 +26,17 @@ class RepositoryTest extends TestCase
         $this->answerRepo = resolve(AnswerRepository::class);
     }
 
+    /**
+     * Should bind correct concrete which is defined in Service
+     */
     public function testAnswerRepoShouldBeInstanceOfEloquentRepository()
     {
         $this->assertInstanceOf('Kuhdo\Survey\Repositories\Answer\EloquentAnswerRepository', $this->answerRepo);
     }
 
+    /**
+     * Should fetch latest answer of a voter (User)
+     */
     public function testGetLatestAnswerOfUser()
     {
         /** @var User $user */
@@ -46,5 +52,40 @@ class RepositoryTest extends TestCase
         $latestAnswer->save();
 
         $this->assertTrue($latestAnswer->is($this->answerRepo->getLatest($user)));
+    }
+
+    /**
+     * Should fetch latest answer of certain question
+     */
+    public function testGetLatestAnswerOfUserForCertainQuestion()
+    {
+        /** @var User $user */
+        $user = User::create();
+        $question = $this->createQuestion();
+
+        $firstAnswer = $this->createAnswer([
+            'question_id' => $question->id
+        ]);
+        $firstAnswer->model()->associate($user);
+        $firstAnswer->save();
+
+
+        /* Create latest answer of certain question */
+        $latestAnswerOfQuestion = $this->createAnswer([
+            'question_id' => $question->id
+        ]);
+        $latestAnswerOfQuestion->model()->associate($user);
+        $latestAnswerOfQuestion->created_at = Carbon::now()->addMinute();
+        $latestAnswerOfQuestion->save();
+
+
+        /* Create latest answer not related to mentioned question */
+        $latestAnswerOfAll = $this->createAnswer();
+        $latestAnswerOfAll->model()->associate($user);
+        $latestAnswerOfAll->created_at = Carbon::now()->addHour();
+        $latestAnswerOfAll->save();
+
+        $this->assertTrue($latestAnswerOfAll->is($this->answerRepo->getLatest($user)));
+        $this->assertTrue($latestAnswerOfQuestion->is($this->answerRepo->getLatestOfQuestion($user, $question)));
     }
 }
